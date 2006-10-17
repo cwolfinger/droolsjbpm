14441846189058b649b84ea322b4d0cbaaaa0e29
Philippe Ombredanne <pombredanne@nexb.com>
2006-10-17 23:50:30 +0000

    JBRULES-517: removed unused xxxtestBasic method and import on commons collections. This can no longuer work now that Jung dependencies are removed.
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/branches/3.0.x@6874 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-core/src/test/java/org/drools/util/BinaryHeapPriorityQueueTest.java
+++ drools-core/src/test/java/org/drools/util/BinaryHeapPriorityQueueTest.java
@@ -2,13 +2,13 @@ package org.drools.util;
 
 /*
  * Copyright 2005 JBoss Inc
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,7 +23,6 @@ import java.util.Random;
 
 import junit.framework.TestCase;
 
-import org.apache.commons.collections.BinaryHeap;
 
 public class BinaryHeapPriorityQueueTest extends TestCase {
     public void testOptimised() {
@@ -61,37 +60,4 @@ public class BinaryHeapPriorityQueueTest extends TestCase {
         System.out.println( "elapsedDequeue = " + elapsedDequeue );
     }
 
-    public void xxxtestBasic() {
-        final Random random = new Random();
-        final List items = new LinkedList();
-
-        final BinaryHeap queue = new BinaryHeap();
-
-        for ( int i = 0; i < 100000; ++i ) {
-            items.add( new LongQueueable( random.nextLong() ) );
-        }
-
-        final long startEnqueue = System.currentTimeMillis();
-
-        for ( final Iterator i = items.iterator(); i.hasNext(); ) {
-            queue.add( i.next() );
-        }
-
-        final long elapsedEnqueue = System.currentTimeMillis() - startEnqueue;
-
-        final long startDequeue = System.currentTimeMillis();
-
-        for ( final Iterator i = items.iterator(); i.hasNext(); ) {
-            queue.remove( i.next() );
-        }
-
-        //        while (!queue.isEmpty()) {
-        //            queue.pop();
-        //        }
-
-        final long elapsedDequeue = System.currentTimeMillis() - startDequeue;
-
-        System.out.println( "elapsedEnqueue = " + elapsedEnqueue );
-        System.out.println( "elapsedDequeue = " + elapsedDequeue );
-    }
 }
