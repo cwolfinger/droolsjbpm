0fd094e22a4e9cfbad7fa204f05cb38271b7847e
Fernando Meyer <fmcamargo@gmail.com>
2007-12-03 13:25:25 +0000

    SOA-193 - Pattern matching does not allow spaces
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/branches/Branch_4_0_2_SOA_4_2@16984 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-compiler/src/test/java/org/drools/integrationtests/MiscTest.java
+++ drools-compiler/src/test/java/org/drools/integrationtests/MiscTest.java
@@ -3067,6 +3067,8 @@ public class MiscTest extends TestCase {
                                            12 );
         final Cheese stilton2 = new Cheese( "stilton2",
                                             12 );
+        final Cheese agedStilton = new Cheese( "aged stilton",
+                                            12 );
         final Cheese brie = new Cheese( "brie",
                                         10 );
         final Cheese brie2 = new Cheese( "brie2",
@@ -3077,6 +3079,7 @@ public class MiscTest extends TestCase {
                                                10 );
         workingMemory.insert( stilton );
         workingMemory.insert( stilton2 );
+        workingMemory.insert( agedStilton );
         workingMemory.insert( brie );
         workingMemory.insert( brie2 );
         workingMemory.insert( muzzarella );
@@ -3084,13 +3087,15 @@ public class MiscTest extends TestCase {
 
         workingMemory.fireAllRules();
 
-        assertEquals( 2,
+        assertEquals( 3,
                       list.size() );
 
         assertEquals( stilton,
                       list.get( 0 ) );
         assertEquals( brie,
                       list.get( 1 ) );
+        assertEquals( agedStilton,
+                      list.get( 2 ) );
     }
 
     public void testAutomaticBindings() throws Exception {
@@ -4409,5 +4414,4 @@ public class MiscTest extends TestCase {
                       0,
                       list.size() );
     }
-
 }
