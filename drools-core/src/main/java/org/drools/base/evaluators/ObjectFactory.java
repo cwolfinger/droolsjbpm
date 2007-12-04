d418b173266c2ece0db8d589d8141c0e17646cec
Fernando Meyer <fmcamargo@gmail.com>
2007-12-04 17:16:30 +0000

    SOA-208 - NullPointerException at JavaConsequenceBuilder.java:54 on RHS for simplest of consequences related to JBRULES-1313
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/branches/Branch_4_0_2_SOA_4_2@17023 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-core/src/main/java/org/drools/base/evaluators/ObjectFactory.java
+++ drools-core/src/main/java/org/drools/base/evaluators/ObjectFactory.java
@@ -565,16 +565,6 @@ public class ObjectFactory
             if( arg0 instanceof String ) {
                 return arg0.equals( arg1.toString() );
             }
-            if( arg0 instanceof Boolean ) {
-                if( arg1 instanceof String ) {
-                    return ((Boolean)arg0).booleanValue() == Boolean.valueOf( (String)arg1 ).booleanValue();
-                }
-            }
-            if( arg0 instanceof Character ) {
-                if( arg1 instanceof String && ((String) arg1).length() == 1 ) {
-                    return ((Character)arg0).charValue() == ((String)arg1).charAt( 0 );
-                }
-            }
             return arg0.equals( arg1 );
         }
     }
@@ -625,11 +615,7 @@ public class ObjectFactory
                 }
                 
             }
-            try {
-                return ((Comparable)arg0).compareTo( arg1 );
-            } catch ( ClassCastException cce ) {
-                throw new ClassCastException( "Not possible to compare a "+arg0.getClass()+" with a "+arg1.getClass());
-            }
+            return ((Comparable)arg0).compareTo( arg1 );
         }
     }
     
