26ee3dccd8d0fed18802c2aab13c494172ba95d5
Mark Proctor <mdproctor@gmail.com>
2008-03-14 13:53:25 +0000

    JBRULES-1415 Certain uses of from causes NullPointerException in WorkingMemoryLogger -fixed nullpointer in logger for derived facthandles.
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/branches/4.0.x@18967 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-compiler/src/test/java/org/drools/integrationtests/MiscTest.java
+++ drools-compiler/src/test/java/org/drools/integrationtests/MiscTest.java
@@ -81,6 +81,8 @@ import org.drools.TestParam;
 import org.drools.WorkingMemory;
 import org.drools.Cheesery.Maturity;
 import org.drools.audit.WorkingMemoryFileLogger;
+import org.drools.audit.WorkingMemoryInMemoryLogger;
+import org.drools.audit.WorkingMemoryLogger;
 import org.drools.base.ClassObjectFilter;
 import org.drools.common.AbstractWorkingMemory;
 import org.drools.common.InternalFactHandle;
@@ -1118,6 +1120,42 @@ public class MiscTest extends TestCase {
 		builder.addPackage(descr);
 		Package pkg = builder.getPackage();
 		pkg.checkValidity();
+	}
+	
+	public void testFromDeclarationWithWorkingMemoryLogger() throws Exception {
+	        String rule = "package org.test;\n";
+	        rule += "import org.drools.Cheesery\n";
+	        rule += "import org.drools.Cheese\n";
+	        rule += "global java.util.List list\n";
+	        rule += "rule \"Test Rule\"\n";
+	        rule += "when\n";
+	        rule += "    $cheesery : Cheesery()\n";
+	        rule += "    Cheese( $type : type) from $cheesery.cheeses\n";
+	        rule += "then\n";
+	        rule += "    list.add( $type );\n";
+	        rule += "end";
+
+	        final PackageBuilder builder = new PackageBuilder();
+	        builder.addPackageFromDrl( new StringReader( rule ));
+	        final Package pkg = builder.getPackage();
+
+	        final RuleBase ruleBase = getRuleBase();
+	        ruleBase.addPackage(pkg);
+	        final StatefulSession session = ruleBase.newStatefulSession();
+	        
+	        WorkingMemoryInMemoryLogger logger = new WorkingMemoryInMemoryLogger( session );
+	        List list = new ArrayList();
+	        session.setGlobal( "list", list );
+	        
+	        Cheesery cheesery = new Cheesery();
+	        cheesery.addCheese(  new Cheese("stilton", 22) );
+	        
+	        session.insert( cheesery );
+	        
+	        session.fireAllRules();	       	        
+	        
+	        assertEquals( 1, list.size());
+	        assertEquals( "stilton", list.get(0));
 	}
 
 	public void testWithInvalidRule() throws Exception {
