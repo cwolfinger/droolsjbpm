dfa65d70c82ef3d98239c177b72b375fdf7275da
Mark Proctor <mdproctor@gmail.com>
2008-03-26 17:24:17 +0000

    JBRULES-1533 WorkingMemoryLogger implementations should take WorkingMemoryEventManager as a constructor
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/branches/4.0.x@19248 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-core/src/main/java/org/drools/audit/WorkingMemoryConsoleLogger.java
+++ drools-core/src/main/java/org/drools/audit/WorkingMemoryConsoleLogger.java
@@ -1,12 +1,13 @@
 package org.drools.audit;
 
 import org.drools.WorkingMemory;
+import org.drools.WorkingMemoryEventManager;
 import org.drools.audit.event.LogEvent;
 
 public class WorkingMemoryConsoleLogger extends WorkingMemoryLogger {
 
-	public WorkingMemoryConsoleLogger(WorkingMemory workingMemory) {
-		super(workingMemory);
+	public WorkingMemoryConsoleLogger(final WorkingMemoryEventManager workingMemoryEventManager) {
+		super(workingMemoryEventManager);
 	}
 	
 	public void logEventCreated(LogEvent logEvent) {
