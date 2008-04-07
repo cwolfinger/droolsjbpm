29f571f608b007d32b6d9965f8e34bb3fd3e2a64
Edson Tirelli <ed.tirelli@gmail.com>
2008-04-07 18:18:30 +0000

    JBRULES-1543: hardcoding tests to run only with JDT
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/branches/Branch_4_0_2_SOA_4_2@19463 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-compiler/src/test/java/org/drools/integrationtests/MiscTest.java
+++ drools-compiler/src/test/java/org/drools/integrationtests/MiscTest.java
@@ -114,6 +114,7 @@ import org.drools.lang.descr.PackageDescr;
 import org.drools.lang.descr.RuleDescr;
 import org.drools.rule.InvalidRulePackage;
 import org.drools.rule.Package;
+import org.drools.rule.builder.dialect.java.JavaDialect;
 import org.drools.rule.builder.dialect.java.JavaDialectConfiguration;
 import org.drools.spi.Activation;
 import org.drools.spi.ConsequenceExceptionHandler;
@@ -1340,7 +1341,12 @@ public class MiscTest extends TestCase {
     }
 
     public void testWithInvalidRule() throws Exception {
-        final PackageBuilder builder = new PackageBuilder();
+        PackageBuilderConfiguration conf = new PackageBuilderConfiguration();
+
+        // this test must run only with JDT... JANINO has problems with this test
+        JavaDialectConfiguration javaconf = (JavaDialectConfiguration) conf.getDialectConfiguration( "java" );
+        javaconf.setCompiler( JavaDialectConfiguration.ECLIPSE );
+        final PackageBuilder builder = new PackageBuilder( conf );
         builder.addPackageFromDrl( new InputStreamReader( getClass().getResourceAsStream( "invalid_rule.drl" ) ) );
         final Package pkg = builder.getPackage();
         // Mark: please check if the conseqeuence/should/shouldn't be built
@@ -4690,7 +4696,6 @@ public class MiscTest extends TestCase {
 
     public void testAlphaEvalWithOrCE() throws Exception {
         PackageBuilderConfiguration conf = new PackageBuilderConfiguration();
-        conf.setDumpDir( new File("./target") );
         final PackageBuilder builder = new PackageBuilder( conf );
         builder.addPackageFromDrl( new InputStreamReader( getClass().getResourceAsStream( "test_AlphaEvalWithOrCE.drl" ) ) );
         final Package pkg = builder.getPackage();
