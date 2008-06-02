1f32b1b546a5b5d581d69b7dd92793211e5d1504
Michael Neale <michael.neale@gmail.com>
2008-06-02 02:44:41 +0000

    making close method public
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/branches/4.0.x@20250 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-jbrms/src/main/java/org/drools/brms/server/repository/RulesRepositoryManager.java
+++ drools-jbrms/src/main/java/org/drools/brms/server/repository/RulesRepositoryManager.java
@@ -1,13 +1,13 @@
 package org.drools.brms.server.repository;
 /*
  * Copyright 2005 JBoss Inc
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -39,13 +39,13 @@ import org.jboss.seam.security.Identity;
 public class RulesRepositoryManager {
 
     private static String READ_ONLY_USER = "anonymous";
-    
-    @In 
+
+    @In
     BRMSRepositoryConfiguration repositoryConfiguration;
-    
+
     private RulesRepository repository;
-    
-    
+
+
     @Create
     public void create() {
         String userName = READ_ONLY_USER;
@@ -54,20 +54,20 @@ public class RulesRepositoryManager {
         }
         if (userName == null) {
             userName = READ_ONLY_USER;
-        }        
+        }
         repository = new RulesRepository(repositoryConfiguration.newSession(userName) );
     }
-    
+
     @Unwrap
     public RulesRepository getRepository() {
         return repository;
     }
-    
+
     @Destroy
-    void close() {
+    public void close() {
         repository.logout();
     }
-    
 
-    
+
+
 }
