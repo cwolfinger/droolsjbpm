55ad073113884baec338a2c52d9664dbaf6673da
Davide Sottara <dsotty@gmail.com>
2009-04-20 17:17:29 +0000

    moved examples into appropriate sub-project
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/branches/DroolsChance@26159 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ drools-core/src/main/java/org/drools/base/evaluators/ImperfectBinaryEvaluatorDefinition.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package org.drools.base.evaluators;
+
+import org.drools.RuntimeDroolsException;
+import org.drools.common.InternalWorkingMemory;
+import org.drools.rule.VariableRestriction.VariableContextEntry;
+import org.drools.spi.FieldValue;
+import org.drools.spi.InternalReadAccessor;
+
+
+
+/**
+ * @author davide
+ *
+ */
+public interface ImperfectBinaryEvaluatorDefinition 
+	extends EvaluatorDefinition {
+	
+	
+
+}
