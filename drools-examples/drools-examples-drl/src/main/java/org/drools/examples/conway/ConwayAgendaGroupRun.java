fd0e9112fbaed02d72851f24c0fd331620f0dc95
Davide Sottara <dsotty@gmail.com>
2009-05-22 03:23:04 +0000

    Major update
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/branches/DroolsChance@26669 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-examples/drools-examples-drl/src/main/java/org/drools/examples/conway/ConwayAgendaGroupRun.java
+++
@@ -1,9 +0,0 @@
-package org.drools.examples.conway;
-
-public class ConwayAgendaGroupRun extends AbstractRunConway {
-
-	public static void main(final String[] args) {
-        ConwayAgendaGroupRun.start( AbstractRunConway.AGENDAGROUP );
-    }
-
-}
--- drools-examples/drools-examples-drl/src/main/java/org/drools/examples/conway/ConwayRuleFlowGroupRun.java
+++
@@ -1,9 +0,0 @@
-package org.drools.examples.conway;
-
-public class ConwayRuleFlowGroupRun extends AbstractRunConway {
-	
-    public static void main(final String[] args) {
-        ConwayRuleFlowGroupRun.start( AbstractRunConway.RULEFLOWGROUP );
-    }
-
-}
